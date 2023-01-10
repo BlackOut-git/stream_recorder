134e974b8d1eb1e85361f6f035e3f922ca6b6301
BlackOut <yass@kakao.com>
2023-01-11 03:36:59 +0900

    %time% 수정

--- recorder_s.bat
+++ recorder_s.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :run
-streamlink --http-cookie "auth-token={토큰}" "{링크}" best -o "{author}_%time%_{category}_{title}.mp4" --twitch-disable-hosting --twitch-disable-ads
+streamlink --http-cookie "auth-token={토큰}" "{링크}" best -o "{author}_%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%_{category}_{title}.mp4" --twitch-disable-hosting --twitch-disable-ads
 rem {토큰}: twitch.tv 로그인 -> F12 -> Application -> Cookies -> auth-token 값 입력
 timeout 10
 goto run
